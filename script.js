f376fd7a1c83014d1498bee9b7385a114e06a7bb
Jaime Leon <s19120178@alumnos.itsur.edu.mx>
2023-09-28 03:15:24 -0600

    Agregando validacion
    se agrego la validacion cuando se cumpla 4 fichas en linea mande un modal

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Función para crear la cuadrícula
     function createGrid() {
-        for (let row = 0; row < 7; row++) { // Cambiamos de 6 a 7 filas
+        for (let row = 0; row < 7; row++) {
             cells[row] = [];
-            for (let col = 0; col < 6; col++) { // Mantenemos 6 columnas
+            for (let col = 0; col < 6; col++) {
                 const cell = document.createElement('div');
                 cell.classList.add('cell');
                 cell.dataset.row = row;
@@ -28,6 +28,74 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    // Función para verificar si hay una línea ganadora
+    function checkWinner(row, col) {
+        const directions = [[1, 0], [0, 1], [1, 1], [1, -1]]; // Direcciones para comprobar
+
+        for (const [dx, dy] of directions) {
+            let count = 1;
+            for (let i = 1; i <= 3; i++) {
+                const newRow = row + i * dx;
+                const newCol = col + i * dy;
+                if (newRow >= 0 && newRow < 7 && newCol >= 0 && newCol < 6 &&
+                    cells[newRow][newCol].classList.contains(currentPlayer)) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            for (let i = 1; i <= 3; i++) {
+                const newRow = row - i * dx;
+                const newCol = col - i * dy;
+                if (newRow >= 0 && newRow < 7 && newCol >= 0 && newCol < 6 &&
+                    cells[newRow][newCol].classList.contains(currentPlayer)) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            if (count >= 4) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Función para mostrar un modal de felicitaciones
+    function showWinnerModal() {
+        const winnerModal = document.getElementById('winner-modal');
+        const winnerText = document.getElementById('winner-text');
+        winnerText.innerText = `¡Felicidades ${currentPlayer}! ¡Ganaste el juego!`;
+
+        const backdrop = document.createElement('div');
+        backdrop.classList.add('modal-backdrop');
+        document.body.appendChild(backdrop);
+
+        winnerModal.style.display = 'block';
+
+        // Cerrar modal al hacer clic fuera de él
+        backdrop.addEventListener('click', function() {
+            winnerModal.style.display = 'none';
+            backdrop.remove();
+        });
+    }
+
+    
+    // Función para dejar caer una ficha en la columna seleccionada
+    function dropPiece(col) {
+        for (let row = 6; row >= 0; row--) {
+            if (!cells[row][col].classList.contains('red') && !cells[row][col].classList.contains('yellow')) {
+                cells[row][col].classList.add(currentPlayer);
+                if (checkWinner(row, col)) {
+                    showWinnerModal();
+                }
+                currentPlayer = (currentPlayer === 'red') ? 'yellow' : 'red';
+                break;
+            }
+        }
+    }  
+
     // Función para limpiar la cuadrícula
     function clearGrid() {
         while (gameBoard.firstChild) {
@@ -35,16 +103,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // Función para dejar caer una ficha en la columna seleccionada
-    function dropPiece(col) {
-        for (let row = 6; row >= 0; row--) {
-            if (!cells[row][col].classList.contains('red') && !cells[row][col].classList.contains('yellow')) {
-                cells[row][col].classList.add(currentPlayer);
-                currentPlayer = (currentPlayer === 'red') ? 'yellow' : 'red'; // Alternar jugador
-                break;
-            }
-        }
-    }
     // Escuchar el evento de clic en el botón de inicio
     startButton.addEventListener('click', function () {
         gameBoard.style.display = 'grid'; // Mostrar el tablero
