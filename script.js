938d1627708c015ffeb57e2638d40630b3b935ff
Jaime Leon <s19120178@alumnos.itsur.edu.mx>
2023-09-28 02:51:37 -0600

    arreglado el bug de tablero
    se soluciono el tablero que tenia error

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const gameBoard = document.querySelector('.game-board');
+    const gameBoard = document.getElementById('board');
+    const cells = [];
+    let currentPlayer = 'red';
+    let gameOver = false;
     const startButton = document.getElementById('start-button');
     const resetButton = document.getElementById('reset-button');
     const modoRadios = document.querySelectorAll('input[name="modoJuego"]');
+    
 
-    // Ocultar el tablero y deshabilitar el botón de reinicio al cargar
-    gameBoard.style.display = 'none';
-    resetButton.disabled = true;
+    // Función para crear la cuadrícula
+    function createGrid() {
+        for (let row = 0; row < 6; row++) {
+            cells[row] = [];
+            for (let col = 0; col < 6; col++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.dataset.row = row;
+                cell.dataset.col = col;
+                gameBoard.appendChild(cell);
+                cells[row][col] = cell;
+            }
+        }
+    }
+
+    // Función para limpiar la cuadrícula
+    function clearGrid() {
+        while (gameBoard.firstChild) {
+            gameBoard.removeChild(gameBoard.firstChild);
+        }
+    }
 
     // Escuchar el evento de clic en el botón de inicio
     startButton.addEventListener('click', function() {
         gameBoard.style.display = 'grid'; // Mostrar el tablero
-        this.style.display = 'none'; // Ocultar el botón de inicio
+        startButton.style.display = 'none'; // Ocultar el botón de inicio
         resetButton.disabled = false; // Habilitar el botón de reinicio
         modoRadios.forEach(radio => radio.disabled = true); // Bloquear radios
+        
+        clearGrid(); // Limpiar el tablero antes de crear una nueva cuadrícula
+        createGrid(); // Llamar a la función para crear la cuadrícula
     });
 
     // Escuchar el evento de clic en el botón de reinicio
@@ -22,5 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
         startButton.style.display = 'block'; // Mostrar el botón de inicio
         resetButton.disabled = true; // Deshabilitar el botón de reinicio
         modoRadios.forEach(radio => radio.disabled = false); // Desbloquear radios
+        clearGrid(); // Limpiar el tablero cuando se reinicia el juego
     });
+
 });
